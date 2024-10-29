e8a36cd63de9197f8ded9d5827180e7a16edaa4c
Andrey Lazurko <rasgis@yandex.ru>
2024-10-29 14:54:23 +0300

    React.createElement

--- quest-app/src/App.js
+++ quest-app/src/App.js
@@ -1,27 +1,40 @@
+import React from 'react';
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
 	const thisYear = new Date().getFullYear(); // императивный
 	// весь return это декларативный стиль
-	return ( 
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div className="year">{thisYear}</div>
-			</header>
-		</div>
+	return React.createElement(
+		'div', 
+		{ className: 'App' },
+		React.createElement(
+			'header',
+			{ className: 'App-header' },
+			React.createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo'
+			}),
+			React.createElement(
+				'p',
+				null,
+				'Edit ',
+				React.createElement('code', null, 'src/App.js'),
+				' and save to reload.'
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer'
+				},
+				'Learn React'
+			),
+			React.createElement('div', { className: 'year' }, thisYear)
+		)
 	);
 }
 
